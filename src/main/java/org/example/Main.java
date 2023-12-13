846960a4e0fe440bc8fbaf356e96f84c2addba3e
Kristina <tinaore@mail.ru>
2023-12-13 15:07:48 +0300

    first commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String[] names = {
+                "Бладшот",
+                "Вперед",
+                "Отель <Белград>",
+                "Джентельмены",
+                "Человек-невидимка",
+                "Тролли.Мировой тур",
+                "Номер один"
+        };
+    }
+}
